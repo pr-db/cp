426723f09fbd7f28e64a71b34a871186a87d395c
pr-db <prakhar.dub@gmail.com>
2022-01-02 11:06:39 +0530

    stage 3 ques 1

--- foobar/Stage-3/find-the-access-codes/solution.py
+++ foobar/Stage-3/find-the-access-codes/solution.py
@@ -3,26 +3,14 @@ def solution(l):
     j = 0
     fct = 0
     while(j < len(l)):
-
-        for i in range(0,j+1,1):
+        for i in range(0, j, 1):
             if l[j] % l[i] == 0 and j != i:
                 ct[j] += 1
                 fct += ct[i]
-            elif l[i] > l[j]:
-                break
         j += 1
-    print(ct)
     return fct
 
 
-l = [1, 1,1,1]
-lr = [1, 2, 3, 4, 5, 6]
-
+l = [1, 2, 4, 8, 9, 10]
 ans = solution(l)
 print(ans)
-
-# l.sort(reverse=True) 
-# lr = l[:]
-# print (lr)
-# l.sort()
-# print(lr)
--- foobar/Stage-3/find-the-access-codes/tempCodeRunnerFile.py
+++ foobar/Stage-3/find-the-access-codes/tempCodeRunnerFile.py
@@ -1,8 +1,14 @@
-j =0
-    # while(j < len(l)):
-    #     for i in l:
-    #         if lr[j] % i == 0 and lr[j] != i:
-    #             fct+=ct[l.index(i)]
-    #         elif i > lr[j]:
-    #             break
-    #     j += 1
+def solution(l):
+#     ct = [0] * len(l)
+#     j = 1
+#     fct = 0
+#     while(j < len(l)):
+#         for i in range(0,j,1):
+#             if l[j] % l[i] == 0 and j != i:
+#                 ct[j] += 1
+#                 fct += ct[i]
+#             elif l[i] > l[j]:
+#                 break
+#         j += 1
+#     print(ct)
+#     return fct
