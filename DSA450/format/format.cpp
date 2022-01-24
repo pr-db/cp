d3ad10792d53f027d3094bfd63d2f5dba3f2641e
PRAy-D <learn.pd9913@gmail.com>
2022-01-24 22:17:12 +0530

    dsa450 try

---
+++ DSA450/format/format.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int func(int a[], int n)
+{
+	int ans;
+
+	return ans;
+}
+int main()
+{
+	int n, k;
+	int a[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
+	n = sizeof(a) / sizeof(a[0]);
+	k = 5;
+	int ans = func(a, n);
+	cout << ans;
+	return 0;
+}
