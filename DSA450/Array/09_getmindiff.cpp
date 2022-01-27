2b17ea039c9254ca4d90a214ec44054866a11303
pr-db <prakhar.dub@gmail.com>
2022-01-27 22:35:40 +0530

    dsa450 1 ques, cf 1 ques try

--- DSA450/Array/09_getmindiff.cpp
+++ DSA450/Array/09_getmindiff.cpp
@@ -10,21 +10,18 @@ using namespace std;
 
 int getmindiff(int a[], int n, int k)
 {
-	int ans;
-	int avg = accumulate(a, a + n, 0) / n;
-	for (int i = 0; i < n; i++)
+	int ans = a[n - 1] - a[0];
+	int smallest = a[0] + k;
+	int largest = a[n - 1] - k;
+	int mini, maxi;
+	for (int i = 0; i < n - 1; i++)
 	{
-		if (a[i] < avg)
-			a[i] += k;
-		else if (a[i] > avg)
-			if (a[i] - k >= 0)
-				a[i] -= k;
-			else
-				a[i] += k;
-		else
-			a[i] += k;
+		mini = min(smallest, a[i + 1] - k);
+		maxi = max(largest, a[i] + k);
+		if(mini<0)
+			continue;
+		ans = min(ans, maxi - mini);
 	}
-	ans = avg;
 	return ans;
 }
 int main()
