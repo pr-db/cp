d89e18d25df04a8e5786efc8a53e41688d7aab45
pr-db <prakhar.dub@gmail.com>
2022-02-24 08:41:35 +0530

    CF ed round 123

---
+++ CodeForces/Format/format.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t > 0)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << endl;
+        }
+
+        t--;
+    }
+    return 0;
+}
