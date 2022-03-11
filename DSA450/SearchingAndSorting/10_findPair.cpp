e7b41a218b96a72dfe44ea8de0cb09ef27c631b4
pr-db <prakhar.dub@gmail.com>
2022-03-11 09:36:05 +0530

    Removed

--- DSA450/SearchingAndSorting/10_findPair.cpp
+++ DSA450/SearchingAndSorting/10_findPair.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
-
+typedef long long ll;
 using namespace std;
 
-int func(int a[], int n)
+int findPair(int a[], int size, int n)
 {
-	int ans;
+	int ans=0;
+	sort(a, a + n);
 	for (int i = 0; i < n; i++)
 	{
+
 	}
 	return ans;
 }
@@ -16,7 +18,7 @@ int main()
 	int a[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
 	n = sizeof(a) / sizeof(a[0]);
 	k = 5;
-	int ans = func(a, n);
+	int ans = findPair(a, n, k);
 	cout << ans;
 	return 0;
 }
