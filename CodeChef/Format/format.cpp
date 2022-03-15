bbfc3bc565159ae9e4b4975caa16c7edc169aeae
pr-db <prakhar.dub@gmail.com>
2022-03-15 17:26:32 +0530

    added fastio

--- CodeChef/Format/format.cpp
+++ CodeChef/Format/format.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 typedef long long int ll;
 using namespace std;
-
+#define FastIO                        \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);                    \
+	cout.tie(NULL)
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    ll t;
-    cin >> t;
-    while (t--)
-    {
-        ll n, k=0;
-        cin >> n ;
+	FastIO;
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		ll n, k = 0;
+		cin >> n;
 		vector<ll> a(n);
 		for (int i = 0; i < n; i++)
-        {
+		{
 			cin >> a[i];
 		}
-
-    }
-    return 0;
+	}
+	return 0;
 }
