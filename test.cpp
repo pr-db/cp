441dc83d95ddb06029dcf45afa611be5e3f4b2b8
pr-db <prakhar.dub@gmail.com>
2022-07-27 09:13:07 +0530

    k cons kadane

--- test.cpp
+++ test.cpp
@@ -5,6 +5,45 @@ using namespace std;
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);                    \
 	cout.tie(NULL)
+
+long long maxSubSumKConcat(vector<int> &arr, int n, int k)
+{
+	long long cs = 0;
+	long long ms = INT64_MIN;
+	for (long long i = 0; i < (n); i++)
+	{
+		cs = cs + arr[i % n];
+		ms = max(ms, cs);
+		if (cs < 0)
+			cs = 0;
+	}
+	cs = 0;
+	long long ms2 = INT64_MIN;
+	for (long long i = 0; i < (min(2, k) * n); i++)
+	{
+		cs = cs + arr[i % n];
+		ms2 = max(ms2, cs);
+		if (cs < 0)
+			cs = 0;
+	}
+	cs = 0;
+	long long ms3 = INT64_MIN;
+	for (long long i = 0; i < (min(3, k) * n); i++)
+	{
+		cs = cs + arr[i % n];
+		ms3 = max(ms3, cs);
+		if (cs < 0)
+			cs = 0;
+	}
+
+	if (ms2 > ms)
+		if (ms3 > ms2)
+			ms = ms2+ ((k - 2) * (ms3 - ms2));
+	if (ms3 == ms2)
+		ms = ms2;
+
+	return ms;
+}
 int main()
 {
 	FastIO;
@@ -12,11 +51,12 @@ int main()
 	cin >> t;
 	while (t--)
 	{
-		ll n, k = 0;
-		cin >> n;
-		vector<ll> a(n);
+		int n, k = 0;
+		cin >> n>>k;
+		vector<int> a(n);
 		for (ll i = 0; i < n; i++)
 			cin >> a[i];
+		cout << maxSubSumKConcat(a, n, k);
 		cout << "\n";
 	}
 	return 0;
