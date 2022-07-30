434dee97abad97469a1ece182b47b2b0c3cd22b2
pr-db <prakhar.dub@gmail.com>
2022-07-31 00:30:55 +0530

    lis

--- LeetCode/Solve/300.cpp
+++ LeetCode/Solve/300.cpp
@@ -16,24 +16,13 @@ public:
 	}
 	int lis(vector<int> &a, int i, int j)
 	{
-		if (i >= n)
+		if (i == n)
 			return 0;
 		if (dp[j] >= 0)
 			return dp[j];
+		int take = 0, donttake = lis(a, i + 1, j);
 		if (a[i] > a[j])
-			dp[j] = max(dp[j], 1 + lis(a, i + 1, i));
-		dp[j] = max(dp[j], lis(a, i + 1, j));
-		return dp[j];
-	}
-	int solve(vector<int> &a, int i, int prev_i)
-	{
-		if (i >= n)
-			return 0;
-		if (dp[prev_i] != -1)
-			return dp[prev_i];
-		int take = 0, dontTake = solve(a, i + 1, prev_i);
-		if (a[i] > a[prev_i])
-			take = 1 + solve(a, i + 1, i);
-		return dp[prev_i] = max(take, dontTake);
+			take = lis(a, i + 1, i) + 1;
+		return dp[j] = max(take, donttake);
 	}
 };
