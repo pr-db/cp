8c29cf65658b5207e60ff54d1bb7cd64771e3e05
pr-db <prakhar.dub@gmail.com>
2022-08-15 18:31:03 +0530

    daily 5

---
+++ LeetCode/August22/377.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+class Solution
+{
+public:
+	vector<int> dp;
+
+	int combinationSum4(vector<int> &nums, int target)
+	{
+		dp = vector<int>(target + 1, -1);
+		return comb(nums, nums.size() - 1, target);
+	}
+	int comb(vector<int> &nums, int n, int rem)
+	{
+		if (rem == 0)
+			return 1;
+		if (n < 0)
+			return 0;
+		if (dp[rem] >= 0)
+			return dp[rem];
+		if (rem - nums[n] >= 0)
+			return dp[rem] = comb(nums, nums.size() - 1, rem - nums[n]) + comb(nums, n - 1, rem);
+		return dp[rem] = comb(nums, n - 1, rem);
+	}
+};
