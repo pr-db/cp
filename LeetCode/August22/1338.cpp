3236922ace7754a0292de433fb488cb53fc7fa19
pr-db <prakhar.dub@gmail.com>
2022-08-18 13:41:13 +0530

    daily 18

---
+++ LeetCode/August22/1338.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+class Solution
+{
+public:
+	int minSetSize(vector<int> &a)
+	{
+		map<int, int> m;
+		vector<int> v;
+		for (auto u : a)
+			m[u]++;
+		for (auto u : m)
+			v.push_back(u.second);
+		sort(v.begin(), v.end(), greater<int>());
+		int n = a.size() / 2, ans = 0;
+		for (auto u : v)
+		{
+			n -= u;
+			ans++;
+			if (n <= 0)
+				break;
+		}
+		return ans;
+	}
+};
