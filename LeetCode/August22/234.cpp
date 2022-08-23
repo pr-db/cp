1e07651ee2cb3f3e50ef5d917984fdff082c4a8c
pr-db <prakhar.dub@gmail.com>
2022-08-23 16:42:34 +0530

    daily 23

---
+++ LeetCode/August22/234.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+	bool isPalindrome(ListNode *head)
+	{
+		ListNode *s = head, *f = head, *ps = NULL, *temp;
+
+		while (f && f->next)
+		{
+			f = f->next->next;
+			temp = s->next;
+			s->next = ps;
+			ps = s;
+			s = temp;
+		}
+		if (f)
+			s = s->next;
+		while (s)
+		{
+			if (s->val != ps->val)
+				return false;
+			s = s->next;
+			ps = ps->next;
+		}
+		return true;
+	}
+};
